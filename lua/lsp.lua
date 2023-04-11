6ccc0f3ae23acabdb2eaec6315fa39b0dc6e5eee
yarob <yarob.021@gmail.com>
2023-04-11 20:28:19 +0200

    2023-04-11 20:28

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -108,6 +108,16 @@ lspconfig['omnisharp'].setup{
 
 }
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = false,
+	underline = true,
+	update_in_insert = false,
+	signs = false,
+  }
+)
+
+
 lspconfig['rust_analyzer'].setup{
 	autostart = false,
 	on_attach = on_attach,
